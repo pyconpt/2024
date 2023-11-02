927d4a67ca62707daf326f3f01be2bd8564bc703
Telmo Guerra <tguerra@evolutio.pt>
2023-11-02 14:57:27 +0000

    first commit

---
+++ pycon_portugal_2024/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pycon_portugal_2024.users.models import User
+from pycon_portugal_2024.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
